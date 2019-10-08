5a89321aa6ee0daaf28ce1863dbedf50ef61d50c
Denilson Perez Junior <denilson.perez@kplay.com.br>
2019-10-08 17:05:09 -0300

    Ajustes leitura

--- gedaci.marilia.unesp.br/module/Quadro/src/Controller/QuadroLeituraController.php
+++ gedaci.marilia.unesp.br/module/Quadro/src/Controller/QuadroLeituraController.php
@@ -66,25 +66,25 @@ class QuadroLeituraController extends AbstractActionController
         
         $response = $this->getResponse();
         $request = $this->getRequest();
+        $post_data = $this->params()->fromPost();
         
         if($request->isPost()){
             $params = array(
                 'usuario'           => $sessao->cod_usuario,
-                'base'              => $this->params()->fromPost('add_base', ''), 
-                'data_pesquisa'     => $this->params()->fromPost('add_datapesq', ''), 
-                'titulo_periodico'  => $this->params()->fromPost('add_periodico', ''), 
-                'ano'               => $this->params()->fromPost('add_ano', ''), 
-                'mes'               => $this->params()->fromPost('add_mes', ''), 
-                'volume'            => $this->params()->fromPost('add_volume', ''), 
-                'numero'            => $this->params()->fromPost('add_numero', ''), 
-                'titulo_artigo'     => $this->params()->fromPost('add_artigo', ''), 
-                'autor'             => $this->params()->fromPost('add_autor', ''), 
-                'pagina_inicial'    => $this->params()->fromPost('add_pginicial', ''), 
-                'pagina_final'      => $this->params()->fromPost('add_pgfim', ''), 
+                'base'              => $post_data['add_base'], 
+                'data_pesquisa'     => $post_data['add_datapesq'], 
+                'titulo_periodico'  => $post_data['add_periodico'], 
+                'ano'               => $post_data['add_ano'] == '' ? 0 : $post_data['add_ano'], 
+                'mes'               => $post_data['add_mes'] == '' ? 0 : $post_data['add_mes'], 
+                'volume'            => $post_data['add_volume'] == '' ? 0 : $post_data['add_volume'], 
+                'numero'            => $post_data['add_numero'] == '' ? 0 : $post_data['add_numero'],
+                'titulo_artigo'     => $post_data['add_artigo'], 
+                'autor'             => $post_data['add_autor'], 
+                'pagina_inicial'    => $post_data['add_pginicial'] == '' ? 0 : $post_data['add_pginicial'],
+                'pagina_final'      => $post_data['add_pgfim'] == '' ? 0 : $post_data['add_pgfim'],
                 'interesse'         => 1,//$this->params()->fromPost('add_interesse', ''), 
-                'palavra_chave'     => $this->params()->fromPost('add_palavra_chave', ''), 
-                'endereco_acesso'   => $this->params()->fromPost('add_endereco', '')
-                //'arquivo'           => $this->params()->fromPost('arquivo', ''), 
+                'palavra_chave'     => $post_data['add_palavra_chave'], 
+                'endereco_acesso'   => $post_data['add_endereco']
             ); 
             
             $arquivo = $this->params()->fromFiles('add_arquivo', '');
@@ -155,25 +155,26 @@ class QuadroLeituraController extends AbstractActionController
         
         $response = $this->getResponse();
         $request = $this->getRequest();
+        $post_data = $this->params()->fromPost();
         
         if($request->isPost()){
             $params = array(
                 'usuario'           => $sessao->cod_usuario,
-                'cod'               => $this->params()->fromPost('cod'),
-                'base'              => $this->params()->fromPost('base', ''), 
-                'data_pesquisa'     => $this->params()->fromPost('data_pesq', ''), 
-                'titulo_periodico'  => $this->params()->fromPost('titulo_periodico', ''), 
-                'ano'               => $this->params()->fromPost('ano', ''), 
-                'mes'               => $this->params()->fromPost('mes', ''), 
-                'volume'            => $this->params()->fromPost('volume', ''), 
-                'numero'            => $this->params()->fromPost('numero', ''), 
-                'titulo_artigo'     => $this->params()->fromPost('titulo_artigo', ''), 
-                'autor'             => $this->params()->fromPost('autor', ''), 
-                'pagina_inicial'    => $this->params()->fromPost('pginicial', ''), 
-                'pagina_final'      => $this->params()->fromPost('pgfim', ''), 
+                'cod'               => $post_data['cod'],
+                'base'              => $post_data['base'],
+                'data_pesquisa'     => $post_data['data_pesq'],
+                'titulo_periodico'  => $post_data['titulo_periodico'],
+                'ano'               => $post_data['ano'] == '' ? 0 : $post_data['ano'], 
+                'mes'               => $post_data['mes'] == '' ? 0 : $post_data['mes'], 
+                'volume'            => $post_data['volume'] == '' ? 0 : $post_data['volume'], 
+                'numero'            => $post_data['numero'] == '' ? 0 : $post_data['numero'],
+                'titulo_artigo'     => $post_data['titulo_artigo'],
+                'autor'             => $post_data['autor'],
+                'pagina_inicial'    => $post_data['pginicial'] == '' ? 0 : $post_data['pginicial'],
+                'pagina_final'      => $post_data['pgfim'] == '' ? 0 : $post_data['pgfim'],
                 'interesse'         => 1,//$this->params()->fromPost('interesse', ''), 
-                'palavra_chave'     => $this->params()->fromPost('palavra_chave', ''), 
-                'endereco_acesso'   => $this->params()->fromPost('endereco_acesso', '')
+                'palavra_chave'     => $post_data['palavra_chave'],
+                'endereco_acesso'   => $post_data['endereco_acesso']
             ); 
             
             /*
